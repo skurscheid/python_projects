40efd12f9cc9b8cad7359cdfc9a08250b4ffb1c3
Sebastian Kurscheid <genomicsio@gmail.com>
2020-05-20 14:45:45 +1000

    commands for interactive python session to download genomic locations of genes organised into MSigDB pathways.

--- biomart_query.py
+++ biomart_query.py
@@ -1,7 +1,10 @@
 import getopt, sys
 from biomart import BiomartServer
 from io import StringIO
-
+from os import listdir
+from os import walk
+import glob
+import pandas as pd
 
 server = BiomartServer('http://www.ensembl.org/biomart')
 hsap = server.datasets['hsapiens_gene_ensembl']
@@ -30,14 +33,22 @@ def get_bed_data(extGeneNames):
     return(response)
 
 def main():
-    tab1 = pd.read_csv('Z:\scratch\mcf10a-xenografts\gene_sets\GeneSetTest_HALLMARK_Hs_LZtumor_vs_WT4.csv')
-    extGeneNames = tab1.iloc[0].SymName.split(',')
-    resp = get_bed_data(extGeneNames)
-    pd1 = pd.read_csv(StringIO(resp.text), sep = '\t', header = None)
-    strand = pd1[3].transform(lambda x: strand_fix(x))
-    pd1[3] = strand
-    pd1[5] = '.'
-    pd1[[0,1,2,3,5,4]]
-    pd1.sort_values(by=0, inplace=True)
-    pd1.to_csv('Z:\scratch\mcf10a-xenografts\gene_sets\GeneSetTest_HALLMARK_Hs_LZtumor_vs_WT4.bed', sep='\t', header=False, index=False)
+    mypath = '/home/sebastian/Data/Tremethick/Breast/Xenografts/gene_sets/*.csv'
+    myfiles = glob.glob(mypath)
+    
+    for i in myfiles:
+        tab1 = pd.read_csv(i)
+        n = i.split('.')[0]   
+        for index, row in tab1.iterrows():
+            extGeneNames = row['SymName'].split(',')
+            resp = get_bed_data(extGeneNames)
+            pd1 = pd.read_csv(StringIO(resp.text), sep = '\t', header = None)
+            strand = pd1[3].transform(lambda x: strand_fix(x))
+            pd1[3] = strand
+            pd1[5] = '.'
+            pd1[[0,1,2,3,5,4]]
+            pd1.sort_values(by=0, inplace=True)
+            pd1.to_csv(n + '/' + row[0] + '.bed', sep='\t', header=False, index=False)
+
+
 
